c0bb8632d2532161c20f908cf260fe1e14ddd216
Lukasz Kalemba <lukasz.kalemba@appstract.software>
2021-05-09 20:47:03 +0200

    Add container bootup order functionality

--- index.js
+++ index.js
@@ -11,13 +11,21 @@ const app = express();
 
 const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Successfully connected to mongodb'))
-  .catch((e) => console.log(e));
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('Successfully connected to mongodb'))
+    .catch((e) => {
+      console.log(e);
+
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.get('/', (req, res) => {
   res.send('Server running');
